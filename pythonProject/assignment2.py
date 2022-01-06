fa12b3014817e67443fb2cf7b98d19227c069ef8
Dan <navon89@gmail.com>
2022-01-06 17:42:22 +0200

    working on graph

--- pythonProject/assignment2.py
+++ pythonProject/assignment2.py
@@ -3,6 +3,7 @@ import time
 from collections import namedtuple
 
 # from typing import List, Set, Any
+from queue import PriorityQueue
 
 import rospy
 import sys
@@ -36,6 +37,11 @@ def rect_contains(rect, point):
 def draw_point(img, p, color):
     cv.circle(img, tuple(p), 2, color, cv.FILLED, cv.LINE_AA)
 
+def distance(a,b):
+    return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
+
+def is_same_edge(e1,e2):
+    return (e1.p1 == e2.p1 and e1.p2 == e2.p2) or (e1.p1 == e2.p2 and e1.p2 == e2.p1)
 
 class CleaningBlocks:
 
@@ -44,7 +50,9 @@ class CleaningBlocks:
         self.occ_map = occ_map
         self.map_size = occ_map.shape
         self.rect = (0, 0, self.map_size[1], self.map_size[0])
+        self.graph = Graph()
 
+        # find corners
         ret, thresh = cv.threshold(occ_map, 90, 255, 0)
         thresh = np.uint8(thresh)
         self.map_rgb = cv.cvtColor(thresh, cv.COLOR_GRAY2BGR)
@@ -54,12 +62,16 @@ class CleaningBlocks:
         corners = cv.goodFeaturesToTrack(thresh, 25, 0.01, 10)
         self.corners = np.int0(corners)
 
+        # find triangles
         # Create an instance of Subdiv2D
         self.sub_div = cv.Subdiv2D(self.rect)
         # Insert points into sub_div
         self.sub_div.insert(corners)
         # Filter triangles outside the polygon
         self.extract_triangles()
+
+        # Build graph
+        self.graph(len(self.triangles))
 
     def extract_triangles(self):
         initial_triangles = self.sub_div.getTriangleList()
@@ -79,10 +91,14 @@ class CleaningBlocks:
                     center = (np.round((t[0] + t[2] + t[4]) / 3), np.round((t[1] + t[3] + t[5]) / 3))
                     mat = np.array([[t[0], t[1], 1], [t[2], t[3], 1], [t[4], t[5], 1]])
                     area = np.linalg.det(mat) / 2
+
+                    tri_edges = [[pt1,pt2,distance(pt1,pt2)], [pt1,pt3,distance(pt1,pt3)], [pt2,pt3,distance(pt2,pt3)]]
+                    center_edges = [[pt1,center,distance(pt1,center)], [pt1,center,distance(pt1,center)], [pt2,center,distance(pt2,center)]]
                     filtered_triangles.append(Triangle(t, center, area))
+                    self.graph.add_edges(tri_edges)
+                    self.graph.add_edges(center_edges)
 
         self.triangles = filtered_triangles
-
 
     def get_triangles(self):
         return self.triangles
@@ -149,19 +165,53 @@ class CleaningBlocks:
 
         return False
 
-    def sort(self, first_pose):
+    def locate_initial_pose(self, first_pose):
         min_dist = 1000
         ind = 0
         x = first_pose[0]
         y = first_pose[1]
         for (i, triangle) in enumerate(self.triangles):
             c = triangle.center
-            dist = np.sqrt((c[0] - x) ** 2 + (c[1] - y) ** 2)
+            dist = distance(c ,first_pose)
             if dist < min_dist:
                 min_dist = dist
                 ind = i
+        # closest_tri = self.triangles[ind]
+        self.graph.add_starting_point(ind)
 
-        closest_tri=self.triangles[ind]
+    def is_neighbor(self, v_i, u_i):
+        v_edges = self.triangles[v_i].edges
+        u_edges = self.triangles[u_i].edges
+        for e1 in v_edges:
+            for e2 in u_edges:
+                if is_same_edge(e1, e2):
+                    return True
+        return False
+
+    def add_vertices(self):
+        min_dist = 1000
+        ind = 0
+
+        num_tri = len(self.triangles)
+        for (i, triangle) in enumerate(self.triangles):
+            c1 = triangle.center
+
+            for j in range(i+1, num_tri ):
+                if neighbors_triangles(i, j):
+                    c2 = self.triangles[j].center
+                    dist = distance(c1, c2)
+            if dist < min_dist:
+                min_dist = dist
+                ind = i
+        # closest_tri = self.triangles[ind]
+        self.graph.add_starting_point(ind)
+
+    def sort(self, first_pose):
+
+        self.locate_initial_pose(first_pose)
+
+        compute_dist_mat()
+
         t = closest_tri.coordinates
         pt1 = (t[0], t[1])
         pt2 = (t[2], t[3])
@@ -174,7 +224,6 @@ class CleaningBlocks:
         draw_point(img, np.uint32(first_pose), (0, 255, 255))
         cv.imshow('delaunay', self.map_rgb)
         cv.waitKey(0)
-
 
 
 class CostMapUpdater:
@@ -245,42 +294,91 @@ class MapService(object):
         pos = np.array([self.initial_pose.position.x, self.initial_pose.position.y])
         return self.position_to_map(pos)
 
-
 class Graph:
-    def __init__(self, gdict=None):
-        if gdict is None:
-            gdict = {}
-        self.gdict = gdict
+    def __init__(self, num_of_vertices):
+        self.v = num_of_vertices
+        self.edges = [[-1 for i in range(num_of_vertices)] for j in range(num_of_vertices)]
+        self.visited = []
 
-    def edges(self):
-        return self.findedges
+    def add_edges(self, edges):
+        for e in edges:
+            self.add_edge(e[0],e[1],e[2])
 
-    # Add the new edge
-    def add_edge(self, edge):
-        edge = set(edge)
-        (vrtx1, vrtx2) = tuple(edge)
-        if vrtx1 in self.gdict:
-            self.gdict[vrtx1].append(vrtx2)
-        else:
-            self.gdict[vrtx1] = [vrtx2]
+    def add_edge(self, u, v, weight):
+        self.edges[u][v] = weight
+        self.edges[v][u] = weight
 
-    # List the edge names
-    def find_edges(self):
-        edge_name = []  # type: List[Set[Any]]
-        for vrtx in self.gdict:
-            for nxtvrtx in self.gdict[vrtx]:
-                if {nxtvrtx, vrtx} not in edge_name:
-                    edge_name.append({vrtx, nxtvrtx})
+    def dijkstra(graph, start_vertex):
+        D = {v: float('inf') for v in range(graph.v)}
+        D[start_vertex] = 0
 
-        return edge_name
+        pq = PriorityQueue()
+        pq.put((0, start_vertex))
 
-    def get_vertices(self):
-        return list(self.gdict.keys())
+        while not pq.empty():
+            (dist, current_vertex) = pq.get()
+            graph.visited.append(current_vertex)
 
-    # Add the vertex as a key
-    def add_vertex(self, vrtx):
-        if vrtx not in self.gdict:
-            self.gdict[vrtx] = []
+            for neighbor in range(graph.v):
+                if graph.edges[current_vertex][neighbor] != -1:
+                    dist = graph.edges[current_vertex][neighbor]
+                    if neighbor not in graph.visited:
+                        old_cost = D[neighbor]
+                        new_cost = D[current_vertex] + dist
+                        if new_cost < old_cost:
+                            pq.put((new_cost, neighbor))
+                            D[neighbor] = new_cost
+        return D
+#
+# class Graph:
+#     def __init__(self, gdict=None):
+#         if gdict is None:
+#             gdict = {}
+#         self.gdict = gdict
+#
+#         self.start_ind = 0
+#
+#     def add_starting_point(self, start):
+#         self.start_ind = start
+#
+#     def edges(self):
+#         return self.findedges
+#
+#     # Add the new edge
+#     def add_edge(self, edge):
+#         edge = set(edge)
+#         (vrtx1, vrtx2) = tuple(edge)
+#         if vrtx1 in self.gdict:
+#             self.gdict[vrtx1].append(vrtx2)
+#         else:
+#             self.gdict[vrtx1] = [vrtx2]
+#
+#     def add_edges(self, edges):
+#         for e in edges:
+#             self.add_edge(e)
+#
+#     # List the edge names
+#     def find_edges(self):
+#         edge_name = []  # type: List[Set[Any]]
+#         for vrtx in self.gdict:
+#             for nxtvrtx in self.gdict[vrtx]:
+#                 if {nxtvrtx, vrtx} not in edge_name:
+#                     edge_name.append({vrtx, nxtvrtx})
+#
+#         return edge_name
+#
+#     def get_vertices(self):
+#         return list(self.gdict.keys())
+#
+#     # Add the vertex as a key
+#     def add_vertex(self, vrtx):
+#         if vrtx not in self.gdict:
+#             self.gdict[vrtx] = []
+#
+#     def add_vertices(self, vertices):
+#         for (i, v) in enumerate(vertices):
+#             self.add_vertex(i)
+
 
 
 def vacuum_cleaning():
@@ -292,12 +390,14 @@ def inspection():
     print('start inspection')
     raise NotImplementedError
 
+def
 
 # If the python node is executed as main process (sourced directly)
 if __name__ == '__main__':
     rospy.init_node('get_map_example')
     ms = MapService()
-    Triangle = namedtuple('Triangle', ['coordinates', 'center', 'area'])
+    Edge =  namedtuple('Edge', ['p1','p2'])
+    Triangle = namedtuple('Triangle', ['coordinates', 'center', 'area', 'edges'])
 
     cb = CleaningBlocks(ms.map_arr)
 
@@ -310,9 +410,8 @@ if __name__ == '__main__':
 
     triangles = []  # Tom's format
     for triangle in triangle_list:
-        t=triangle.coordinates
+        t = triangle.coordinates
         triangles.append((np.array((t[0], t[1], 0)), np.array((t[2], t[3], 0)), np.array((t[4], t[5], 0))))
-
 
     exec_mode = sys.argv[1]
     print('exec_mode:' + exec_mode)
